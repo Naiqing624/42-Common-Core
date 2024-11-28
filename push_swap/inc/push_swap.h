a48755e874c8eb26ed0df7db81dcbe6de7c480be
Naiqing Cao <nacao@c1r4s1.42luxembourg.lu>
2024-11-28 16:45:11 +0100

    push_swap

---
+++ push_swap/inc/push_swap.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nacao <nacao@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 15:56:04 by nacao             #+#    #+#             */
+/*   Updated: 2024/11/28 16:16:13 by nacao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <limits.h>
+# include "../libft/inc/ft_printf.h"
+# include "../libft/inc/libft.h"
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*prev;
+	struct s_stack_node	*next;
+}	t_stack_node;
+
+#endif
