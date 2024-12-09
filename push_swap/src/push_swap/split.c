ef14e141bc987afd5226b02967009369dc88d1c5
Naiqing Cao <nacao@c1r4s1.42luxembourg.lu>
2024-12-09 13:32:11 +0100

    push_swap

--- push_swap/src/push_swap/split.c
+++ push_swap/src/push_swap/split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nacao <nacao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 15:07:04 by marvin            #+#    #+#             */
-/*   Updated: 2024/12/08 15:07:04 by marvin           ###   ########.fr       */
+/*   Updated: 2024/12/09 10:54:36 by nacao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static int	count_string(char *str, char c)
 	len = 0;
 	while (*str)
 	{
-		inside == false;
+		inside = false;
 		while (*str == c)
 			str++;
 		while (*str != c && *str)
@@ -28,7 +28,7 @@ static int	count_string(char *str, char c)
 			if (!inside)
 			{
 				len++;
-				inside == true;
+				inside = true;
 			}
 			str++;
 		}
@@ -36,15 +36,14 @@ static int	count_string(char *str, char c)
 	return (len);
 }
 
-static char	get_next_word(char *str, char c)
+static char	*get_next_word(char *str, char c)
 {
-	static int	cursor;
+	static int	cursor = 0;
 	int			len;
 	char		*result;
 	int			i;
 
-	cursor == 0;
-	len == 0;
+	len = 0;
 	while (str[cursor] == c)
 		cursor++;
 	while (str[cursor + len] != c && str[cursor + len])
@@ -59,7 +58,48 @@ static char	get_next_word(char *str, char c)
 	return (result);
 }
 
-char	ft_split(char **str, char c)
+char	**ft_ssplit(char *str, char c)
 {
-	
+	char	**result;
+	int		count;
+	int		i;
+
+	count = count_string(str, c);
+	result = malloc(sizeof(char *) * (count + 2));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (count-- > 0)
+	{
+		if (i == 0)
+		{
+			result[i] = (char *)malloc(sizeof(char));
+			if (!result[i])
+				return (NULL);
+			result[i++][0] = '\0';
+		}
+		result[i++] = get_next_word(str, c);
+	}
+	result[i] = NULL;
+	return (result);
 }
+
+/*
+int	main(void)
+{
+	char	input[] = ",Hello,world,Push,Swap";
+	char	delimiter = ',';
+	char	**result = ft_ssplit(input, delimiter);
+	int		i;
+
+	i = 0;
+	while (result[i])
+	{
+		printf("%s", result[i]);
+		free(result[i]);
+		i++;
+	}
+	free(result);
+	return (0);
+}
+*/
