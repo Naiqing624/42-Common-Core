3a342ec5f72484024becdc434546eb38165141b6
naiqing <naiqing.cao24@gmail.com>
2025-01-06 09:14:50 +0100

    so_long

---
+++ so_long/inc/so_long.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/05 20:06:25 by marvin            #+#    #+#             */
+/*   Updated: 2025/01/05 20:06:25 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+// #include <mlx.h>
+// #include <X11/X.h>
+// #include <X11/keysym.h>
+#include "../libft/inc/ft_printf.h"
+#include "../libft/inc/libft.h"
+
+
+
+#endif
